ab87cc34c1b1267b09c3740d44687d04779d07ef
Billy-Hung <hungvm1101@gmail.com>
2024-11-12 21:08:20 +0700

    Delete font/index_script.js

--- font/index_script.js
+++
@@ -1,6 +0,0 @@
-const playBtn = document.getElementById("playBtn");
-const AboutBtn = document.getElementById("AboutBtn");
-
-playBtn.onclick = function(){
-    window.location.href = "in_game.html";
-}
