aa55f1003c7152eb9a5f55cce2efe140ff0b38c8
Billy-Hung <hungvm1101@gmail.com>
2024-11-12 22:01:33 +0700

    Add files via upload

---
+++ about_script.js
@@ -0,0 +1,5 @@
+const returnBtn = document.getElementById("returnBtn");
+
+returnBtn.onclick = function(){
+    window.location.href = "index.html";
+}
---
+++ index_script.js
@@ -0,0 +1,10 @@
+const playBtn = document.getElementById("playBtn");
+const AboutBtn = document.getElementById("AboutBtn");
+
+playBtn.onclick = function(){
+    window.location.href = "in_game.html";
+}
+
+AboutBtn.onclick = function(){
+    window.location.href = "about.html";
+}
